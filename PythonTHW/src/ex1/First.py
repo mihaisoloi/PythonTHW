77e673a52eaaeeef3fb0c97e2996808319831ffa
Mihai Soloi <mihai.soloi@gmail.com>
2011-07-28 19:15:37 +0300

    first commit on the project

---
+++ PythonTHW/src/ex1/First.py
@@ -0,0 +1,13 @@
+'''
+Created on Jul 28, 2011
+
+@author: msoloi
+'''
+print "Hello World!"
+print "Hello World Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+#print "I'd much rather you 'not'."
+#print 'I "said" to not touch this.'
+print "Printing one more line."
